0beb0835aba52be91ba96df549a12679bf171e8c
luischan2 <luischan.com.pa@gmail.com>
2017-07-06 23:46:00 -0500

    add de publicidad

--- UbicatePeru/UbicatePeru/Pmaestra/Default.aspx
+++ UbicatePeru/UbicatePeru/Pmaestra/Default.aspx
@@ -1,5 +1,12 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pmaestra/Maestro.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="UbicatePeru.Pmaestra.Default" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <h1>Oficinas Mas Buscadas</h1>
+    <asp:Repeater ID="Repeater1" runat="server">
+        <ItemTemplate>
+            <div class="oficina">
 
+
+            </div>
+        </ItemTemplate>
+    </asp:Repeater>
 </asp:Content>
--- UbicatePeru/UbicatePeru/Pmaestra/Maestro.Master.designer.cs
+++ UbicatePeru/UbicatePeru/Pmaestra/Maestro.Master.designer.cs
@@ -29,5 +29,23 @@ namespace UbicatePeru.Pmaestra {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
+        /// Control AdRotator1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.AdRotator AdRotator1;
+        
+        /// <summary>
+        /// Control XmlDataSource1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.XmlDataSource XmlDataSource1;
     }
 }
